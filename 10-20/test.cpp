89e128d72d19642f552a7dcdf445b2fd5cdd5035
WZH-moyu <445163140@qq.com>
2019-10-20 11:57:06 +0800

    多态

---
+++ 10-20/test.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<stdlib.h>
+int rand(void);
+using namespace std;
+//class TakeBus
+//{
+//  public:
+//    void TakeBusToSubway()
+//    {
+//      cout<<"go to Subway---->please take bus of 307"<<endl;
+//    }
+//    void TakeBusToStation()
+//    {
+//      cout<<"go to Station---->please Take Bus of 306 or 915"<<endl;
+//    }
+//};
+//class Bus
+//{
+//  public:
+//    virtual void TakeBusToSomewhere(TakeBus & tb)=0;
+//};
+//class Subway:public Bus 
+//{
+//  public:
+//    virtual void TakeBusToSomewhere(TakeBus& tb)
+//    {
+//      tb.TakeBusToSubway();
+//    }
+//};
+//class Station:public Bus 
+//{
+//  public:
+//    virtual void TakeBusToSomewhere(TakeBus& tb) 
+//    {
+//      tb.TakeBusToStation();
+//    }
+//};
+//int main()
+//{
+//  TakeBus tb;
+//  Bus* b=NULL;
+//  for(int i=1;i<=10;i++)
+//  {
+//    if((rand()%i)& 1)
+//      b=new Subway;
+//    else 
+//      b=new Station;
+//  }
+//  b->TakeBusToSomewhere(tb);
+//  delete b;
+//  return 0;
+//}
+
